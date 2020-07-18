6e419a60d31befba38a79131ca59caf622d73cde
Harish Kumar <28harish.kumar@gmail.com>
2020-07-18 17:38:24 +0530

    fix: org.opencv.RNCustomCropPackage not found

---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dependency: {
+    platforms: {
+      android: {
+        packageImportPath: 'import com.customcrop.RNCustomCropPackage;',
+        packageInstance: 'new RNCustomCropPackage()',
+      },
+    },
+  },
+};  
